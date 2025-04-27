fb799e053dcc2a7fb94dbfc322ee824b292341b5
Xerão <mathischerer@gmail.com>
2025-04-27 20:11:12 -0300

    bruh

--- Aula/Atividades/API Pokemon/script.js
+++ Aula/Atividades/API Pokemon/script.js
@@ -1,4 +1,4 @@
-const URL = "https://pokeapi.co/api/v2/pokemon/" + String(Math.floor(Math.random() * 250) + 1);
+const URL = "https://pokeapi.co/api/v2/pokemon/" + String(Math.floor(Math.random() * 1000) + 1);
 const NOME = document.querySelector(".nome");
 const TIPO = document.querySelector(".tipo");
 const IMAGEM = document.querySelector(".imagem");
@@ -12,19 +12,23 @@ fetch(URL)
         }
     })
     .then(data => {
-        NOME.textContent = data.name.toUpperCase();
-
-        let tipos = "";
-        for (let i = 0; i < data.types.length; i++) {
-            const element = data.types[i].type.name;
-            tipos += (i === data.types.length - 1) ? element : element + " - ";
-        }
-        TIPO.textContent = tipos;
-
         const IMG = document.createElement("img");
         IMG.src = data.sprites.front_default;
         IMAGEM.appendChild(IMG);
         IMG.style.width = "100%";
+
+        const BOLA = document.createElement("img");
+        BOLA.src = `icons/balls/${Math.floor(Math.random() * 38) + 1}.png`;
+        NOME.appendChild(BOLA);
+        NOME.innerHTML += data.name.toUpperCase();
+
+        let tipos = "";
+        for (let i = 0; i < data.types.length; i++) {
+            const element = data.types[i].type.name;
+
+            tipos = `<div class="divTipo"><img class="icon" src="./icons/${element}.png"></img><p>${element}</p></div>`
+            TIPO.innerHTML += tipos + "<br>";
+        }
 
         console.log(data)
     })
